78bd921b4ab9cfaf93b1a13c8348b68f3b63065c
Umut Kaya <101326640+Ucodek@users.noreply.github.com>
2022-07-11 17:58:49 +0300

    Update CalFinalCNote.java

--- CalculatingFinalNote/src/CalcFinNote/CalFinalCNote.java
+++ CalculatingFinalNote/src/CalcFinNote/CalFinalCNote.java
@@ -9,19 +9,19 @@ public class CalFinalCNote {
 		int Final;
 		int Last;
 		Vize = Integer.parseInt(JOptionPane.showInputDialog(null, "Please enter your midterm note"));
-		if(Vize>100) { JOptionPane.showMessageDialog(null, "Vize notu 100 den büyük olamaz");
-		JOptionPane.showInputDialog(null, "Vize notunu yeniden gir");}
-		else if(Vize<0) {JOptionPane.showMessageDialog(null, "Vize notu 0dan küçük olamaz");
-		Vize = Integer.parseInt(JOptionPane.showInputDialog("Vize notunu yeniden gir"));
+		if(Vize>100) { JOptionPane.showMessageDialog(null, "The midterm note can't be greater than 100");
+		JOptionPane.showInputDialog(null, "please enter your midterm note again");}
+		else if(Vize<0) {JOptionPane.showMessageDialog(null, "The midterm note cannot be lower than 0");
+		Vize = Integer.parseInt(JOptionPane.showInputDialog("please enter your midterm note again"));
 		JOptionPane.showMessageDialog(null, "Vize has entered");}
 		Final = Integer.parseInt(JOptionPane.showInputDialog("Please enter your Final"));
 		JOptionPane.showMessageDialog(null, "Final has entered");
 		int X= Vize*4/10;
 		int Y= Final*6/10;
 		Last = X+Y;
-		if(Last>=60) {JOptionPane.showMessageDialog(null, Last+ " ile geçtin "+ name +" Baþaracaðýný biliyordum");
+		if(Last>=60) {JOptionPane.showMessageDialog(null, "You passed the course with "Last+ "grade" + name +" I knew you could make it");
 
-	}else {JOptionPane.showMessageDialog(null, Last+" ile Kaldýn "+ name+ " Daha fazla çalýþmalýsýn");
+	}else {JOptionPane.showMessageDialog(null," You failed the course with"+ Last+ name+ " You have to work harder");
 		
 	
 	
